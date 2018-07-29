a192585a0c72d34aeeefa92a167ac770d3257d76
Martin Chapman <martin.chapman@kcl.ac.uk>
2018-07-29 22:51:03 +0100

    Initial Prolog integration tests.

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -1,4 +1,12 @@
 import sys
+from pyswip import Prolog
 
-def helloworld(out):
-    out.write("Hello world of Python\n")
+def helloworld():
+    prolog = Prolog();
+    prolog.assertz("father(michael,john)")
+    prolog.assertz("father(michael,gina)")
+    list(prolog.query("father(michael,X)")) == [{'X': 'john'}, {'X': 'gina'}]
+    for soln in prolog.query("father(X,Y)"):
+        print(soln["X"], "is the father of", soln["Y"])
+    
+helloworld();
