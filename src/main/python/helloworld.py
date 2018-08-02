03294017686f9d9481ad3b01e6c055ce5015f03a
Martin Chapman <martin.chapman@kcl.ac.uk>
2018-08-02 19:59:07 +0100

    Drug queries.

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -3,10 +3,11 @@ from pyswip import Prolog
 
 def helloworld():
     prolog = Prolog();
-    prolog.assertz("father(michael,john)")
-    prolog.assertz("father(michael,gina)")
-    list(prolog.query("father(michael,X)")) == [{'X': 'john'}, {'X': 'gina'}]
-    for soln in prolog.query("father(X,Y)"):
-        print(soln["X"], "is the father of", soln["Y"])
+    prolog.assertz("use_module(library(semweb/turtle))");
+    prolog.assertz("use_module(library(semweb/rdf_http_plugin))");
+    prolog.assertz("use_module(library(semweb/rdf_db))");
+    
+    for soln in prolog.query("rdf_load('https://www.dropbox.com/s/33v1zze5fpbmnzh/model.ttl?raw=1', [format('turtle'), register_namespaces(false), base_uri('http://anonymous.org/vocab/'), graph('http://anonymous.org/vocab')])"):
+        print(soln);
     
 helloworld();
