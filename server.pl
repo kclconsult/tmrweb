2c8ecbaf85908695df3f49fdef4bcbebd810812b
Martin Chapman <contact@martinchapman.co.uk>
2018-09-25 15:54:26 +0100

    Drug post route

--- server.pl
+++ server.pl
@@ -1,16 +1,29 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_client)).
 :- use_module(library(semweb/turtle)).
 :- use_module(library(semweb/rdf_http_plugin)).
 :- use_module(library(semweb/rdf_db)).
 
 :- http_handler(root(interactions), show_interactions, []).     % (1)
+:- http_handler(root(drug), show_drug, []).
+
+:- consult(setup).
 
 server(Port) :-                     % (2)
-        consult(setup),
+        %consult(setup),
         http_server(http_dispatch, [port(Port)]).
 
 show_interactions(_Request) :-                 % (3)
  	inferInternalInteractions,
 	format('Content-type: text/plain~n~n'),
 	rdf_global_id(data:'CIG-OA-HT-DB',Guideline),guideline_recommendations(Guideline, Recommendations),maplist(recommendation_term, Recommendations, Terms),findall(interaction(Interaction,Label,Elems,External),interaction(Recommendations, Interaction, Label, Elems, External),Interactions),with_output_to(codes(Codes), write(Interactions)), format("~s", [Codes]).
+
+show_drug(Request) :-
+        member(method(post), Request), !,
+        http_read_data(Request, Data, []),
+        format('Content-type: text/plain~n~n', []),
+	rdf('http://anonymous.org/data/RecOA-HT-DB-Diuretic', vocab:'aboutExecutionOf', DrugAdministration),
+	rdf('http://anonymous.org/data/ActAdministerThiazide', vocab:'administrationOf', Drug),
+        format(Drug).
+	
--- setup.pl
+++ setup.pl
@@ -3,13 +3,13 @@
 :- use_module(library(semweb/rdf_db)).
 :- use_module(library(semweb/rdfs)).
 
-:- rdf_load('https://www.dropbox.com/s/33v1zze5fpbmnzh/model.ttl?raw=1', [format('turtle'), register_namespaces(false), base_uri('http://anonymous.org/vocab/'), graph('http://anonymous.org/vocab')]).
-:- rdf_load('https://www.dropbox.com/s/7mfhm6ohlon2yj0/model4I%203.0.ttl?raw=1',[format('turtle'), register_namespaces(false),base_uri('http://anonymous.org/vocab4i/'), graph('http://anonymous.org/vocab4i')]).
-:- rdf_load('https://www.dropbox.com/s/ad95snt663lcbvc/CareAction%26DrugTypes.ttl?raw=1',[format('turtle'), register_namespaces(false),base_uri('http://anonymous.org/data/'), graph('http://anonymous.org/CareAction&DrugTypes')]).
-:- rdf_load('https://www.dropbox.com/s/ayt89to51dakjsw/Transition%26SituationTypes.ttl?raw=1',[format('turtle'), register_namespaces(false),base_uri('http://anonymous.org/data/'), graph('http://anonymous.org/Transition&SituationTypes')]).
-:- rdf_load('https://www.dropbox.com/s/ofzeq00zrm1jy3h/CausationBeliefs-Nanopub.trig?raw=1 ', [format('trig'), register_namespaces(false), base_uri('http://anonymous.org/data/'), graph('http://anonymous.org/CausationBeliefs-Nanopub')]).
-:- rdf_load('https://www.dropbox.com/s/fn6j5wqg8clgjfl/Reg%26Norms-Nanopub.trig?raw=1',[format('trig'), register_namespaces(false),base_uri('http://anonymous.org/data/'), graph('http://anonymous.org/Reg&Norms-Nanopub')]).
-:- rdf_load('https://www.dropbox.com/s/1ax2uerv5wzzrj5/MergedReg%26Norms-Nanopub.trig?raw=1',[format('trig'), register_namespaces(false),base_uri('http://anonymous.org/data/'), graph('http://anonymous.org/MergedReg&Norms-Nanopub')]).
+:- rdf_load('/home/martin/CONSULT/Guidelines/dev/rapgmsbgym/schemas/model.ttl', [format('turtle'), register_namespaces(false), base_uri('http://anonymous.org/vocab/'), graph('http://anonymous.org/vocab')]).
+:- rdf_load('/home/martin/CONSULT/Guidelines/dev/rapgmsbgym/schemas/model4I3.0.ttl',[format('turtle'), register_namespaces(false),base_uri('http://anonymous.org/vocab4i/'), graph('http://anonymous.org/vocab4i')]).
+:- rdf_load('/home/martin/CONSULT/Guidelines/dev/rapgmsbgym/guidelines/CareActionDrugTypes.ttl',[format('turtle'), register_namespaces(false),base_uri('http://anonymous.org/data/'), graph('http://anonymous.org/CareAction&DrugTypes')]).
+:- rdf_load('/home/martin/CONSULT/Guidelines/dev/rapgmsbgym/guidelines/TransitionSituationTypes.ttl',[format('turtle'), register_namespaces(false),base_uri('http://anonymous.org/data/'), graph('http://anonymous.org/Transition&SituationTypes')]).
+:- rdf_load('/home/martin/CONSULT/Guidelines/dev/rapgmsbgym/guidelines/CausationBeliefsNanopub.trig', [format('trig'), register_namespaces(false), base_uri('http://anonymous.org/data/'), graph('http://anonymous.org/CausationBeliefs-Nanopub')]).
+:- rdf_load('/home/martin/CONSULT/Guidelines/dev/rapgmsbgym/guidelines/RegNormsNanopub.trig',[format('trig'), register_namespaces(false),base_uri('http://anonymous.org/data/'), graph('http://anonymous.org/Reg&Norms-Nanopub')]).
+:- rdf_load('/home/martin/CONSULT/Guidelines/dev/rapgmsbgym/guidelines/MergedRegNormsNanopub.trig',[format('trig'), register_namespaces(false),base_uri('http://anonymous.org/data/'), graph('http://anonymous.org/MergedReg&Norms-Nanopub')]).
 
 :- include('../rapgmsbgym/lib/interactionRules').
 :- include('../rapgmsbgym/lib/interaction_graph').
